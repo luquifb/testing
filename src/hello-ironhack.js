0c1723520ce3d70c5371ea744563d55427afdbb4
Lucas <lfbodereau@gmail.com>
2017-08-23 16:13:53 +0200

    testing-1

---
+++ src/hello-ironhack.js
@@ -0,0 +1,12 @@
+
+function helloIronhack() {
+  return "Hello Ironhacker!";
+}
+
+function sumArray (array) {
+  return 0;
+}
+
+function sumArray (array) {
+  return (array.length < 1 ? 0 : array[0]);
+}
